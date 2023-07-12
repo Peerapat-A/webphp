f63ec6706c978973d1600c194dfb2ca3f536ce6d
Peerapat-A <6421061002@cdti.ac.th>
2023-07-12 13:27:25 +0700

    update

--- member_update.php
+++ member_update.php
@@ -4,7 +4,7 @@
     $sql = "UPDATE tbl_member SET firstname='$_POST[firstname]', lastname='$_POST[lastname]', email='$_POST[email]', phone='$_POST[phone]', address='$_POST[address]' WHERE id = '$_GET[id]' ";
 
     if (mysqli_query($conn, $sql)) {
-        echo "New record updated";
+        echo "The record has been updated";
         header("location: member_show.php");
       } else {
         echo "Error update fail: " . $sql . "<br>" . mysqli_error($conn);
--- product_update.php
+++ product_update.php
@@ -4,7 +4,7 @@
     $sql = "UPDATE tbl_product SET name='$_POST[name]', price='$_POST[price]', stock='$_POST[stock]', description='$_POST[description]' WHERE id = '$_GET[id]' ";
 
     if (mysqli_query($conn, $sql)) {
-        echo "New record updated";
+        echo "The record has been updated";
         header("location: product_show.php");
       } else {
         echo "Error update fail: " . $sql . "<br>" . mysqli_error($conn);
